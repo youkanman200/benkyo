c30075b299d2e7a7236b2f5d37a4b132c6e827e4
youkanman200 <sec47001@gmail.com>
2017-01-13 22:00:12 +0900

    benkyou

---
+++ staff_add_done.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+
+<?php
+
+try
+{
+
+$staff_name=$_POST['name'];
+$staff_pass=$_POST['pass'];
+
+$staff_name=htmlspecialchars($staff_name);
+$staff_pass=htmlspecialchars($staff_pass);
+
+$dsn = 'mysql:dbname=shop;host=localhost';
+$user = 'root';
+$password = 'aaa123';
+$dbh = new PDO($dsn,$user,$password);
+$dbh->query('SETNAMES utf8');
+
+$sql = 'INSERT INTO mst_staff(name,password) VALUES(?,?)';
+$stmt = $dbh->prepare($sql);
+$data[]=$staff_name;
+$data[]=$staff_pass;
+$stmt->execute($data);
+
+$dbh=null;
+
+print $staff_name;
+print'さんを追加しました。 <br />';
+
+}
+ catch (Exception $e)
+ {
+print'ただいま障害により大変ご迷惑をお掛けしております。';
+exit();
+}
+ ?>
+
+ <a href="staff_list.php">戻る</a>
+</body>
+</html>
